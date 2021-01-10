143b4104438cce73b311480227d42ea480ade2d8
18238831810 <1527583922@qq.com>
2021-01-10 19:13:15 +0800

    添加用户信息api

--- crs-admin/src/main/java/com/cf/crs/service/AccountService.java
+++ crs-admin/src/main/java/com/cf/crs/service/AccountService.java
@@ -3,8 +3,10 @@ package com.cf.crs.service;
 
 import com.alibaba.fastjson.JSON;
 import com.cf.crs.huobi.client.AccountClient;
+import com.cf.crs.huobi.client.req.account.AccountBalanceRequest;
 import com.cf.crs.huobi.constant.HuobiOptions;
 import com.cf.crs.huobi.model.account.Account;
+import com.cf.crs.huobi.model.account.AccountBalance;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,18 @@ public class AccountService {
         if (CollectionUtils.isEmpty(list)) return null;
         log.info("accountList:{}", JSON.toJSONString(list));
         return list.get(0);
+    }
+
+    /**
+     * 获取账户余额
+     * @param apiKey
+     * @param secretKey
+     * @return
+     */
+    public AccountBalance getAccountBalance(String apiKey,String secretKey){
+        AccountClient accountClient = getAccountClient(apiKey, secretKey);
+        Account account = getAccount(apiKey, secretKey);
+        return accountClient.getAccountBalance(AccountBalanceRequest.builder().accountId(account.getId()).build());
     }
 
 
--- crs-admin/src/test/java/com/cf/crs/ApplicationTest.java
+++ crs-admin/src/test/java/com/cf/crs/ApplicationTest.java
@@ -3,6 +3,7 @@ package com.cf.crs;
 
 import com.alibaba.fastjson.JSON;
 import com.cf.crs.huobi.model.account.Account;
+import com.cf.crs.huobi.model.account.AccountBalance;
 import com.cf.crs.service.AccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +19,21 @@ public class ApplicationTest {
     @Autowired
     AccountService accountService;
 
+    /**
+     * 获取用户信息
+     */
     @Test
-    public void test(){
+    public void getAccount(){
         Account account = accountService.getAccount("fa4e6356-67181a8e-dab4c45e6f-dc250", "a8f67e99-2d7ad560-fefabfd7-1ef78");
         System.out.println(JSON.toJSONString(account));
+    }
+
+    /**
+     * 获取用户余额
+     */
+    @Test
+    public void getAccountBalance(){
+        AccountBalance accountBalance = accountService.getAccountBalance("fa4e6356-67181a8e-dab4c45e6f-dc250", "a8f67e99-2d7ad560-fefabfd7-1ef78");
+        System.out.println(JSON.toJSONString(accountBalance));
     }
 }
