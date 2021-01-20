c18ddc7f271bece8238abc8bc03eb2a20c66dc0b
18238831810 <1527583922@qq.com>
2021-01-20 11:16:51 +0800

    订单处理

--- crs-admin/src/main/java/com/cf/crs/service/TradeService.java
+++ crs-admin/src/main/java/com/cf/crs/service/TradeService.java
@@ -6,16 +6,13 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cf.crs.entity.BuyLimit;
 import com.cf.crs.entity.OrderEntity;
 import com.cf.crs.huobi.client.TradeClient;
-import com.cf.crs.huobi.client.req.trade.CreateOrderRequest;
 import com.cf.crs.huobi.constant.HuobiOptions;
-import com.cf.crs.huobi.model.account.Account;
 import com.cf.crs.huobi.model.trade.Order;
 import com.cf.crs.mapper.BuyLimitMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 
@@ -107,7 +104,7 @@ public class TradeService {
      * @return
      */
     public List<BuyLimit> getBuyLimitList(){
-        return buyLimitMapper.selectList(new QueryWrapper<BuyLimit>().eq("status", 0));
+        return buyLimitMapper.selectList(new QueryWrapper<BuyLimit>().lt("status", 2));
     }
 
     /**
@@ -122,11 +119,34 @@ public class TradeService {
             //限价下单，存入数据库
             log.info("查询订单:{},{}",buyLimit.getOrderId(),JSON.toJSONString(order));
             //如果点单状态改变，则走后续步骤 1.订单撤单，则改变订单状态，2.订单成功，则入库持仓表 3.如果还未成功并到达撤单时间，则撤单
+            /*created：已创建，该状态订单尚未进入撮合队列。
+            submitted : 已挂单等待成交，该状态订单已进入撮合队列当中。
+            partial-filled : 部分成交，该状态订单在撮合队列当中，订单的部分数量已经被市场成交，等待剩余部分成交。
+            filled : 已成交。该状态订单不在撮合队列中，订单的全部数量已经被市场成交。
+            partial-canceled : 部分成交撤销。该状态订单不在撮合队列中，此状态由partial-filled转化而来，订单数量有部分被成交，但是被撤销。
+            canceling : 撤销中。该状态订单正在被撤销的过程中，因订单最终需在撮合队列中剔除才会被真正撤销，所以此状态为中间过渡态。
+            canceled : 已撤销。该状态订单不在撮合订单中，此状态订单没有任何成交数量，且被成功撤销。*/
+            String state = order.getState();
+            if ("created".equals(state) || "submitted".equals(state)){
+                //挂单还未成交
+                if (buyLimit.getCancelTime() >= System.currentTimeMillis()){
+                    //超出撤单时间还未成交，进行撤单操作
+                    cancelOrder(tradeClient,buyLimit);
+                }
+            }else if("partial-filled".equals(state)){
+                //部分成交，更新更新订单库，并根据成交量选择挂单卖出
 
 
-            //更改订单状态
-            //buyLimit.setStatus(1);
-            //buyLimitMapper.updateById(buyLimit);
+            }else if("filled".equals(state)){
+                //已成交
+                if(buyLimit.getStatus() == 0){
+                   //一步到位，直接全部成交了。全部卖出并更新买单库
+                }else if(buyLimit.getStatus() == 1){
+                   //分批成交，更新订单库，并全部卖出
+
+                }
+            }
+
         } catch (Exception e) {
             log.info("撤单失败:{}，{}",buyLimit.getOrderId(),JSON.toJSONString(buyLimit));
             log.error(e.getMessage(),e);
@@ -138,10 +158,10 @@ public class TradeService {
     /**
      * 撤单
      * @param buyLimit
+     * @param tradeClient
      * @return
      */
-    public void cancelOrder(BuyLimit buyLimit){
-        TradeClient tradeClient = getTradeClient(buyLimit.getApiKey(),buyLimit.getSecretKey());
+    public void cancelOrder(TradeClient tradeClient,BuyLimit buyLimit){
         try {
             tradeClient.cancelOrder(buyLimit.getOrderId());
             //限价下单，存入数据库
