45ad89128dbce3c6b4cdc0c38c0a460256721a98
Administrator <d>
2021-02-23 10:20:22 +0800

    优化

--- crs-admin/src/main/java/com/cf/crs/service/AbstractHuobiPraramService.java
+++ crs-admin/src/main/java/com/cf/crs/service/AbstractHuobiPraramService.java
@@ -1,6 +1,6 @@
 package com.cf.crs.service;
 
 public interface AbstractHuobiPraramService {
-    String apiKey = "e86f40c1-c7d024fe-ghxertfvbf-02f16",
-            secretKey = "5e8d4b45-1fcd8068-f6a09700-b56b2";
+    String apiKey = "e86f40c1-c7d024fe-ghxertfvbf-02f1611",
+            secretKey = "5e8d4b45-1fcd8068-f6a09700-b56b222";
 }
